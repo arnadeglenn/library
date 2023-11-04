007f8850812e3ec189cba8b9c2bb603c84b0852d
arnadeglenn <125632656+arnadeglenn@users.noreply.github.com>
2023-11-03 23:48:33 -0700

    Addd form validity using JS

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputPages = document.querySelector('#input-pages');
 const inputRead = document.querySelector('#input-read');
 const inputBookButton = document.querySelector('#add-book-form');
 const allDeleteButton = document.querySelectorAll('.delete');
+const form = document.querySelector('.form');
 
 
 addBookButton.addEventListener('click', (e) => {
@@ -23,7 +24,17 @@ closeModal.addEventListener('click', (e) => {
 
 inputBookButton.addEventListener('click', (e) =>{
     e.preventDefault();
-    functionClass.callBothFunctions(e);
+    if (!form.checkValidity()) {
+        if (!inputBook.validity.valid) {
+            validityFunctionCall.bookValidation();
+        } else if (!inputAuthor.validity.valid) {
+            validityFunctionCall.authorValidation();
+        } else if (!inputPages.validity.valid) {
+            validityFunctionCall.pageValidation();
+        }
+    } else {
+        functionClass.callBothFunctions(e);
+    }
 })
 
 pageLibrary.addEventListener('click', (e) => {
@@ -46,26 +57,31 @@ pageLibrary.addEventListener('click', (e) => {
     }
 });
 
+form.addEventListener('submit', (e) => {
+    if (!form.checkValidity()) {
+        e.preventDefault()
+        validityFunctionCall.bookValidation();
+        validityFunctionCall.authorValidation();
+        validityFunctionCall.pageValidation();
+    }
+})
 
-//  function deleteBook(index) {
-//      bookLibrary.splice(index,1);
-//  }
+inputBook.addEventListener('input', (e) =>{
+    validityFunctionCall.bookValidation();
+})
 
-//  function changeReadStatus(index) {
-//     if (bookLibrary[index].read === "true") {
-//         bookLibrary[index].read = "false";
-//     } else if (bookLibrary[index].read === "false") {
-//         bookLibrary[index].read = "true";
-//     };
-//  };
+inputAuthor.addEventListener('input', (e) => {
+    validityFunctionCall.authorValidation();
+})
 
-
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-// }
+inputPages.addEventListener('input', (e) => {
+    if (inputPages.validity.typeMismatch) {
+        inputPages.setCustomValidity('You must enter a number');
+        console.log('not a num')
+    } else {
+        inputPages.setCustomValidity('');
+    }
+})
 
 class Book {
     constructor (name, author, pages, read) {
@@ -96,7 +112,7 @@ class BookFunction {
             let createTitle = document.createElement('h2');
             createBookDiv.appendChild(createTitle);
             createTitle.classList.add('title');
-            createTitle.textContent = `Book: ${bookLibrary[0+i].title}`
+            createTitle.textContent = `Book: ${bookLibrary[0+i].name}`
     
             let createAuthor = document.createElement('p');
             createBookDiv.appendChild(createAuthor);
@@ -155,72 +171,34 @@ class BookFunction {
 
 let functionClass = new BookFunction();
 
-// function addBookToArray() {
-//     let bookName = inputBook.value;
-//     console.log(bookName);
-//     let authorName = inputAuthor.value;
-//     let bookPage = inputPages.value;
-//     let bookRead = inputRead.value;
-//     let bookObject = new Book(bookName, authorName, bookPage, bookRead);
-//     bookLibrary.push(bookObject);
-// }
 
-// function addBookToLibrary() {
-//     for(let i=0; i<bookLibrary.length; i++) {
-//         let createBookDiv = document.createElement('div');
-//         pageLibrary.appendChild(createBookDiv);
-//         createBookDiv.classList.add('book');
+class ValidationFunction {
+    constructor() {};
+    bookValidation() {
+        if (inputBook.validity.valueMissing) {
+            inputBook.setCustomValidity('You must enter a book name');
+            inputBook.reportValidity();
+        } else {
+            inputBook.setCustomValidity('');
+        }
+    }
+    authorValidation() {
+        if (inputAuthor.validity.valueMissing) {
+            inputAuthor.setCustomValidity('You must enter an author name');
+            inputAuthor.reportValidity();
+            console.log('missing author');
+        } else {
+            inputAuthor.setCustomValidity('');
+        }
+    }
+    pageValidation() {
+        if (!inputPages.validity.typeMismatch) {
+            inputPages.setCustomValidity('You must enter a number');
+            inputPages.reportValidity();
+        } else {
+            inputPages.setCustomValidity('');
+        }
+    }
+}
 
-//         let createTitle = document.createElement('h2');
-//         createBookDiv.appendChild(createTitle);
-//         createTitle.classList.add('title');
-//         createTitle.textContent = `Book: ${bookLibrary[0+i].title}`
-
-//         let createAuthor = document.createElement('p');
-//         createBookDiv.appendChild(createAuthor);
-//         createAuthor.classList.add('author');
-//         createAuthor.textContent = `Author: ${bookLibrary[0+i].author}`
-
-//         let createPages = document.createElement('p');
-//         createBookDiv.appendChild(createPages);
-//         createPages.classList.add('pages');
-//         createPages.textContent = `Pages: ${bookLibrary[0+i].pages}`
-
-//         let createRead = document.createElement('p');
-//         createBookDiv.appendChild(createRead);
-//         createRead.classList.add('read');
-//         if (bookLibrary[0+i].read === 'true') {
-//             createRead.textContent = "Read: Yes"
-//         } else {
-//             createRead.textContent = "Read: Not Yet"
-//         };
-
-//         let createButtonDiv = document.createElement('div');
-//         createBookDiv.appendChild(createButtonDiv);
-//         createButtonDiv.classList.add('button-group');
-
-//         let createButtonRead = document.createElement('button');
-//         createButtonDiv.appendChild(createButtonRead);
-//         createButtonRead.classList.add('change-read');
-//         createButtonRead.textContent = 'Read?';
-//         createButtonRead.setAttribute('data-index', i);
-
-//         let createButtonDelete = document.createElement('button');
-//         createButtonDiv.appendChild(createButtonDelete);
-//         createButtonDelete.classList.add('delete');
-//         createButtonDelete.textContent = 'Delete';
-//         createButtonDelete.setAttribute('data-index', i);
-
-//     }
-// }
-
-// function callBothFunctions() {
-//     addBookToArray();
-//     pageLibrary.innerHTML = '';
-//     addBookToLibrary();
-//     modal.close();
-// }
-
-
-
-
+let validityFunctionCall = new ValidationFunction();
